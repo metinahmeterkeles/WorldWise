bdfa5626d8db6c5b233aa6cbc3915cc2dac288fa
Metin Ahmet Erkeleş <erkelesmetin@gmail.com>
2024-03-11 11:23:05 +0300

    project folders added

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
+import CountriesList from "./components/CountriesList";
+import City from "./components/City";
+import Form from "./components/Form";
+import { CitiesProvider } from "./context/CitiesContext";
+
+function App() {
+  return (
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountriesList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
+  );
+}
+
+export default App;
